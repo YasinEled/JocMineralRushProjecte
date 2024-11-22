be7355d4f05d42cf122ae8013a9b8de8781004cb
Alexrupa27 <alexrupa123@gmail.com>
2024-11-22 18:32:26 +0100

    Nubes acabado

---
+++ mineralrush/ESCENAS/OPTIONS/GLOBAL/cloud.gd
@@ -0,0 +1,15 @@
+extends Sprite2D
+var speed: float = 50
+var screen_height: float = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	screen_height = get_viewport_rect().size.y
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position.x += speed * delta
+	if position.x > 1400:
+		position.x = -100
+		position.y = randf() * screen_height
