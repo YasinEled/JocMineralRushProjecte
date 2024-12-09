8179718e503001dcb07405e77d09b0b1e6ea1282
Alexrupa27 <alexrupa123@gmail.com>
2024-12-09 20:15:02 +0100

    popup

---
+++ mineralrush/ESCENAS/GAME/stone_add.gd
@@ -0,0 +1,14 @@
+extends Sprite2D
+@onready var stone_add: Sprite2D = $"."
+@onready var but_stone_add: Button = $butStoneADD
+@onready var mineral: Label = $CanvasLayer/Mineral
+
+var piedras = 0
+
+# Se ejecuta cuando la escena se carga
+func _ready() -> void:
+	mineral.text += piedras
+
+func _on_but_stone_add_pressed() -> void:
+	stone_add.visible = false
+	but_stone_add.visible = false
