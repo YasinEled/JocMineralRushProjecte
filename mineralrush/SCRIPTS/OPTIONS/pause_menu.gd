47b2584b2f32c3020a4e4ef1a4c506ff1cd073c8
oriolpujadas <oriol.pujadasmontane@gmail.com>
2024-12-09 19:11:39 +0100

    arreglado pause
    falta fondo paused

--- mineralrush/SCRIPTS/OPTIONS/pause_menu.gd
+++ mineralrush/SCRIPTS/OPTIONS/pause_menu.gd
@@ -1,9 +1,24 @@
 extends Control
 
+
+@onready var menu_options: OptionsMenu = $"../MENU-OPTIONS"
+@onready var pause_menu: Control = $"."
+
 @onready  var main = $"../../../"
 # Called when the node enters the scene tree for the first time.
+
+var paused = false
+
+signal exit_options_menu
+
 func _ready() -> void:
 	pass 
+
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("pause"):
+		paused = !paused  # Alternar el estado de pausa
+		pause_menu.visible = paused  # Mostrar u ocultar el menú
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,7 +27,7 @@ func _process(delta: float) -> void:
 
 
 func _on_resume_pressed() -> void:
-	get_tree().change_scene_to_file("res://ESCENAS/GAME/game.tscn")
+	pause_menu.visible = false
 
 
 func _on_quit_pressed() -> void:
@@ -20,4 +35,11 @@ func _on_quit_pressed() -> void:
 
 
 func _on_options_pressed() -> void:
-	get_tree().change_scene_to_file("res://ESCENAS/OPTIONS/GLOBAL/menu_options.tscn")
+	menu_options.visible = true
+	pause_menu.visible = false
+	
+	
+
+func _on_menuoptions_exit_options_menu() -> void:
+	menu_options.visible = false
+	pause_menu.visible = true
